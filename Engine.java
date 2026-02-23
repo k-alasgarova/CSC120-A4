cf11caffb3d8c230373d3242a4f1a0ad3526b7a1
k-alasgarova <kalasgarova@smith.edu>
2026-02-23 16:54:33 -0500

    engine version 1

--- Engine.java
+++ Engine.java
@@ -1,3 +1,37 @@
-public class Engine {
+public class Engine implements EngineRequirements {
+    private FuelType fuelType;
+    private double currentFuel;
+    private double maxFuel;
 
+
+    public Engine(FuelType fuelType, double currentFuel, double maxFuel) {
+        this.fuelType = fuelType;
+        this.currentFuel = currentFuel;
+        this.maxFuel = maxFuel;
+    }
+
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public double getMaxFuel() {
+        return maxFuel;
+    }
+
+    public double getCurrentFuel() {
+        return currentFuel;
+    }
+
+    public void refuel() {
+        currentFuel = maxFuel;
+    }
+
+    public Boolean go() {
+        if (currentFuel > 0) {
+            currentFuel -= 10;
+            System.out.println("Fuel remaining: " + currentFuel);
+            return true;
+        }
+        return false;
+    }
 }
